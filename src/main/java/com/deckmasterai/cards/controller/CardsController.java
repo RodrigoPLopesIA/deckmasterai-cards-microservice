35f2e0ffa109eae09ed17103baa815b416901e34
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2026-02-21 14:07:54 -0300

    refactor: add profileId to ensure the authentication and refactor the service tests

--- src/main/java/com/deckmasterai/cards/controller/CardsController.java
+++ src/main/java/com/deckmasterai/cards/controller/CardsController.java
@@ -9,6 +9,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -21,9 +23,9 @@ public class CardsController {
     private final CardService cardService;
 
     @GetMapping
-    public ResponseEntity<Page<CardResponse>> index(Pageable pageable) {
+    public ResponseEntity<Page<CardResponse>> index(Pageable pageable, @AuthenticationPrincipal Jwt jwt) {
 
-        var cards = cardService.getCards(pageable);
+        var cards = cardService.getCards(pageable, jwt.getSubject());
         return ResponseEntity.ok().body(cards);
     }
 
@@ -38,8 +40,9 @@ public class CardsController {
     // POST /cards
     @PostMapping()
     public ResponseEntity<CardResponse> createCard(
-            @RequestBody @Valid CardRequest card) {
-        var cardCreated = this.cardService.create(card);
+            @RequestBody @Valid CardRequest card, @AuthenticationPrincipal Jwt jwt) {
+
+        var cardCreated = this.cardService.create(card, jwt.getSubject());
         return ResponseEntity.ok().body(cardCreated);
     }
 
@@ -47,36 +50,37 @@ public class CardsController {
     @PutMapping(value = "/{id}")
     public ResponseEntity<CardResponse> updateCard(
             @PathVariable String id,
-            @RequestBody @Valid CardRequest updatedCard) {
-        var updatedCardResponse = this.cardService.update(id, updatedCard);
+            @RequestBody @Valid CardRequest updatedCard,
+            @AuthenticationPrincipal Jwt jwt) {
+        var updatedCardResponse = this.cardService.update(id, updatedCard, jwt.getSubject());
         return ResponseEntity.ok().body(updatedCardResponse);
     }
 
     // GET /cards/{id}
     @GetMapping("/{id}")
-    public ResponseEntity<CardResponse> getCardById(@PathVariable String id) {
-        var card = this.cardService.getCardById(id);
+    public ResponseEntity<CardResponse> getCardById(@PathVariable String id, @AuthenticationPrincipal Jwt jwt) {
+        var card = this.cardService.getCardById(id, jwt.getSubject());
         return ResponseEntity.ok().body(card);
     }
 
     // GET /cards/{id}/decks
     @GetMapping("/{id}/decks")
-    public ResponseEntity<?> getDecksByCardId(@PathVariable String id) {
-        var decks = cardService.getDecksByCardId(id);
+    public ResponseEntity<?> getDecksByCardId(@PathVariable String id, @AuthenticationPrincipal Jwt jwt) {
+        var decks = cardService.getDecksByCardId(id, jwt.getSubject());
         return ResponseEntity.ok().body(decks);
     }
 
     // GET /cards/{id}/decks
     @GetMapping("/{id}/decks/{deckId}")
-    public ResponseEntity<?> getDeckByIdByCardId(@PathVariable String id, @PathVariable String deckId) {
-        var decks = cardService.getDeckByIdByCardId(id, deckId);
+    public ResponseEntity<?> getDeckByIdByCardId(@PathVariable String id, @PathVariable String deckId, @AuthenticationPrincipal Jwt jwt) {
+        var decks = cardService.getDeckByIdByCardId(id, deckId, jwt.getSubject());
         return ResponseEntity.ok().body(decks);
     }
 
     // DELETE /cards/{id}
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteCard(@PathVariable String id) {
-        this.cardService.delete(id);
+    public ResponseEntity<Void> deleteCard(@PathVariable String id, @AuthenticationPrincipal Jwt jwt) {
+        this.cardService.delete(id, jwt.getSubject());
         return ResponseEntity.noContent().build();
     }
 
