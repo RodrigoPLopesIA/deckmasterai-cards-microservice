f33243740df79d9ceb7c1dc767cf182e2f11dc76
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2026-02-20 23:42:04 -0300

    test: create update card test

--- src/test/java/com/deckmasterai/cards/services/CardServiceTest.java
+++ src/test/java/com/deckmasterai/cards/services/CardServiceTest.java
@@ -28,6 +28,7 @@ import org.springframework.data.domain.PageRequest;
 
 
 import java.util.List;
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 public class CardServiceTest {
@@ -98,17 +99,11 @@ public class CardServiceTest {
                 MonsterType.SPELLCASTER,
                 List.of(MonsterSubType.NORMAL)
         );
-
-
-
-        /*Mockito.when(cardMapper.cardRequestToCard(Mockito.any(CardRequest.class))).thenReturn(card);
-        Mockito.when(cardMapper.cardToCardRequest(Mockito.any(Card.class))).thenReturn(cardRequest);
-        Mockito.when(cardMapper.updateCardFromRequest(Mockito.any(CardRequest.class), Mockito.any(Card.class))).thenReturn(card);*/
     }
 
     @Test
     @DisplayName("GET all cards")
-    void getAllCards() {
+    void getAllCardsTest() {
 
         // Arrange
         Page<Card> page = new PageImpl<>(List.of(card));
@@ -130,5 +125,34 @@ public class CardServiceTest {
 
         Mockito.verify(cardRepository)
                 .findAll(PageRequest.of(0, 10));
+    }
+
+    @Test
+    @DisplayName("POST create a card")
+    void createCardTest(){
+        Mockito.when(cardMapper.cardRequestToCard(Mockito.any(CardRequest.class))).thenReturn(card);
+        Mockito.when(cardMapper.cardToCardResponse(Mockito.any(Card.class)))
+                .thenReturn(cardResponse);
+        Mockito.when(cardRepository.save(Mockito.any(Card.class))).thenReturn(card);
+
+        var card = cardService.create(cardRequest);
+
+        Assertions.assertThat(card).isNotNull();
+
+    }
+
+    @Test
+    @DisplayName("PUT update a card")
+    void updateCardTest(){
+        Mockito.when(cardMapper.updateCardFromRequest(Mockito.any(CardRequest.class), Mockito.any(Card.class))).thenReturn(card);
+        Mockito.when(cardMapper.cardToCardResponse(Mockito.any(Card.class)))
+                .thenReturn(cardResponse);
+        Mockito.when(cardRepository.findById(Mockito.anyString())).thenReturn(Optional.of(card));
+        Mockito.when(cardRepository.save(Mockito.any(Card.class))).thenReturn(card);
+
+        var card = cardService.update("1234", cardRequest);
+
+        Assertions.assertThat(card).isNotNull();
+
     }
 }
