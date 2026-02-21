125cc6b48c3957bdb1966a06d391e17f9f1b52f3
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2026-02-20 21:25:56 -0300

    feat: annotate path variables in DeckClient methods for improved API clarity

--- src/main/java/com/deckmasterai/cards/client/DeckClient.java
+++ src/main/java/com/deckmasterai/cards/client/DeckClient.java
@@ -2,15 +2,16 @@ package com.deckmasterai.cards.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @FeignClient(name = "deck-service", url = "http://localhost:8081")
 public interface DeckClient {
     
     @GetMapping("/api/v1/card/{cardId}/decks")
-    public String getDecksByCardId(String cardId);
+    public String getDecksByCardId(@PathVariable String cardId);
 
 
     @GetMapping("/api/v1/card/{cardId}/decks/{deckId}")
-    public String getDeckByIdByCardId(String cardId, String deckId);
+    public String getDeckByIdByCardId(@PathVariable String cardId, @PathVariable String deckId);
 }
